81c7b3bf1c802de22b32e64a4ea7ecbd800d590d
Chui Mei Hua <toptech20190609@gmail.com>
2020-01-09 11:08:45 +0800

    cart page

--- eshop/cart/views.py
+++ eshop/cart/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
-
+from . import cart
 # Create your views here.
 def show_cart(request, template_name="cart.html"):
-    cart_item_count = cart.cart_item_count(request)
+    cart_item_count = cart.cart_distinct_item_count(request)
     page_title = 'Shopping Cart'
     return render(request, template_name=template_name, context = locals())
