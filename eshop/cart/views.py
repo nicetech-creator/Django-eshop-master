8fd5e30489a2387445db3da55e642d733644e5c8
Chui Mei Hua <toptech20190609@gmail.com>
2020-01-10 12:32:50 +0800

    cart page with cart items

--- eshop/cart/views.py
+++ eshop/cart/views.py
@@ -3,5 +3,7 @@ from . import cart
 # Create your views here.
 def show_cart(request, template_name="cart.html"):
     cart_item_count = cart.cart_distinct_item_count(request)
+    cart_items = cart.get_cart_items(request)
     page_title = 'Shopping Cart'
     return render(request, template_name=template_name, context = locals())
+
